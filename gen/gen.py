7e52cc5fb9d3244a39d670e0791fc7aadb007af9
oxalica <oxalicc@pm.me>
2019-12-16 16:37:20 +0800

    Plot result images

--- gen/gen.py
+++ gen/gen.py
@@ -6,17 +6,17 @@ import random
 import re
 import math
 
-N = 4
+N = 8
 
 BENCHES = {}
-SPEED_RANGE = list(range(1, 250, 25))
+SPEED_RANGE = list(range(1, 150, 15))
 
 RE_RESULT = re.compile(rb'(?s).*finished:\s*(\d+)\naverage Delay:\s*(\S+)\n')
 
 class Result(NamedTuple):
     finished: int
     delay: float
-    hot: List[int]
+    hot: List[List[int]]
 
 def run_net(name: str, net: Net) -> Result:
     assert net.n == N
@@ -40,13 +40,12 @@ def run_net(name: str, net: Net) -> Result:
     ]
     sub = run_process(args, check=True, stdout=PIPE, stderr=PIPE)
     m = RE_RESULT.match(sub.stdout)
-    hot = [0] * (net.n * net.n)
+    hot = [[0] * net.n for _ in range(net.n)]
     for route_log in sub.stderr.decode().split('\n'):
         if route_log:
             x, y, cnt = map(int, route_log.split(' '))
-            idx = x * net.n + y
-            assert hot[idx] == 0
-            hot[idx] = cnt
+            assert hot[x][y] == 0
+            hot[x][y] = cnt
 
     return Result(
         finished=int(m[1]),
@@ -54,21 +53,69 @@ def run_net(name: str, net: Net) -> Result:
         hot=hot,
     )
 
+def save_plot(
+    result_dir: Path,
+    speed: List[float],
+    finished: List[float],
+    delay: List[float],
+    hot: List[List[float]],
+):
+    import matplotlib.pyplot as plt
+    assert len(speed) == len(finished) == len(delay)
+    n = len(hot)
+    assert all(len(row) == n for row in hot)
+
+    fig, ax1 = plt.subplots()
+    color = 'tab:red'
+    ax1.set_xlabel('Packet sending frequency (packet/cycle)')
+    ax1.set_ylabel('Throughput (packet/cycle)', color=color)
+    ax1.plot(speed, finished, 'o-', color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('Delay (cycle)', color=color)
+    ax2.plot(speed, delay, 'o-', color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    plt.title('Network efficiency')
+    plt.savefig(result_dir / 'efficiency.png')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(hot, cmap=plt.cm.hot_r)
+    plt.colorbar(im)
+    plt.title('Packets transmitted by each router')
+    plt.savefig(result_dir / 'heatmap.png')
+
 def bench(name: str, speeds: List[int]):
+    assert speeds
     def decorator(gen: Callable[[int], Net]) -> Callable[[int], Net]:
         def bench_runner():
             print(f'Benching {name}: ', end='', flush=True)
-            with open(Path('./result') / f'{name}.txt', 'w') as f:
+            result_dir = Path('./result') / name
+            result_dir.mkdir(exist_ok=True)
+
+            finished: List[float] = []
+            delay: List[float] = []
+            hot: List[List[float]] = None
+            with open(result_dir / 'ret.txt', 'w') as f:
                 n = len(speeds)
                 for i, speed in enumerate(speeds):
                     fmt = f'{i}/{n} '
                     print(fmt + '\b' * len(fmt), end='', flush=True)
+
                     net = gen(speed)
                     ret = run_net(name, net)
+
+                    finished.append(ret.finished)
+                    delay.append(ret.delay)
+                    hot = ret.hot
                     f.write(f'{speed} {ret.finished} {ret.delay}\n')
-                    f.write(' '.join(str(cnt) for cnt in ret.hot))
+                    f.write(' '.join(str(cnt) for row in ret.hot for cnt in row))
                     f.write('\n')
                     f.flush()
+
+            save_plot(result_dir, speeds, finished, delay, hot)
             print('Done ')
         assert name not in BENCHES
         BENCHES[name] = bench_runner
